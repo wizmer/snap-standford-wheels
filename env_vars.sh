114f264efb383088865b6c5c9012dafb29d749a8
Benoît Coste <benoit.coste@epfl.ch>
2020-06-12 15:46:14 +0200

    aoeuaoue

--- env_vars.sh
+++ env_vars.sh
@@ -6,6 +6,9 @@ if [ -z "$IS_OSX" ]; then
     # Override the flag in Makefile.config
     export IFLAGS3=-I/opt/python/cp36-cp36m/include/python3.6m
 else
-    python-config
+    python-config --includes
+    python3-config --includes
+    python-config --ldflags
+    python3-config --ldflags
     export SNAPROOT=${TRAVIS_BUILD_DIR}/snap
 fi
