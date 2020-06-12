5f40d9057e23424be36da532fa53d7562aa705d4
Benoît Coste <benoit.coste@epfl.ch>
2020-06-12 11:22:33 +0200

    Fix flags

--- env_vars.sh
+++ env_vars.sh
@@ -1,2 +1,8 @@
-export SNAPROOT=${SNAPROOT:=/io/snap}
-export IFLAGS3=-I/opt/python/cp36-cp36m/include/python3.6m
+if [ -z "$IS_OSX" ]; then
+    export SNAPROOT=/io/snap
+
+    # Override the flag in Makefile.config
+    export IFLAGS3=-I/opt/python/cp36-cp36m/include/python3.6m
+else
+    export SNAPROOT=${TRAVIS_BUILD_DIR}/snap
+fi
